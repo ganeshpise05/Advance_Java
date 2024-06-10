1504338030b9dfafaa2e93fa63709538005e9e34
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-10 11:32:47 +0530

    Decision statement:If-eg6 ;

---
+++ Control_Flow_Statement/src/decision_Statement/IfNegativeNum.java
@@ -0,0 +1,14 @@
+package decision_Statement;
+
+//Example 6: Checking if a number is negative
+public class IfNegativeNum {
+
+	public static void main(String[] args) {
+		 int num = -5;
+		 if (num<0) {
+			System.out.println("The given number is negative number.");
+		}
+
+	}
+
+}
