f26097ac4e4a02ebb7c13144237d83364693604c
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-10 10:50:50 +0530

    Loop statement : Do -eg1 ;

---
+++ Control_Flow_Statement/src/loop_Statement/Do1.java
@@ -0,0 +1,12 @@
+package loop_Statement;
+
+public class Do1 {
+            public static void main(String[] args) {
+            	 int i = 0;
+   	          while(i<5){
+   				System.out.println("Please do Study");
+   				i++;
+   			    } 
+   	          System.out.println("loop exit ..");
+         }
+}
