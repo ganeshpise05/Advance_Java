ea958d63b48b034f4eba83cf0cea69cb255533e8
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-10 11:30:48 +0530

    Decision statement:If-eg5 ;

---
+++ Control_Flow_Statement/src/decision_Statement/IfCheckVowel.java
@@ -0,0 +1,13 @@
+package decision_Statement;
+
+//Example 5: Checking if a character is a vowel
+public class IfCheckVowel {
+
+	public static void main(String[] args) {
+		 char ch = 'a';
+		 if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
+			System.out.println("The given character is vowel.");
+		}
+	}
+
+}
