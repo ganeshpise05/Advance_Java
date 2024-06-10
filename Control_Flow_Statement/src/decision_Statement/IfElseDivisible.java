54f4a3baa3501904bcb7c84d3e56e1704e6d2400
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-10 12:05:09 +0530

    Decision statement:If_else-eg4 ;

---
+++ Control_Flow_Statement/src/decision_Statement/IfElseDivisible.java
@@ -0,0 +1,24 @@
+package decision_Statement;
+import java.util.Scanner;
+//Example 4: Checking if a number is divisible by 5
+public class IfElseDivisible {
+
+	public static void main(String[] args) {
+		 
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number:");
+		int num = sc.nextInt();
+		
+		if (num%5==0) {
+			
+			System.out.println("The given number is divisible by 5.");
+			
+		} else {
+			
+			System.out.println("The given number is not divisible by 5.");
+
+		}
+
+	}
+
+}
