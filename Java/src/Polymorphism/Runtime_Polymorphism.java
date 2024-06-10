02dbf361294e10c48c9cb94c7c7584636bbba467
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-10 13:57:52 +0530

    Theory program of Runtime Polymorphism.

---
+++ Java/src/Polymorphism/Runtime_Polymorphism.java
@@ -0,0 +1,17 @@
+package Polymorphism;
+
+public class Runtime_Polymorphism {
+
+	public static void main(String[] args) {
+		 
+		System.out.print("Runtime Polymorphism can be achieved by using two approaches : ");
+		System.out.println("1> Upcasting");
+		System.out.println("                                                               2> Method Overriding");
+		System.out.println("Method Overriding : Non-static methods with same name created in different class i.e parent and child class. And it is also Known as Compile Time Polymorphism(CT Poly)");
+		System.out.println("Method Shadowing : Static methods with same name created in different class i.e parent and child class. And it is also Known as Run Time Polymorphism(RT Poly)");
+		System.out.println("*_*_*_*_*_*_");
+		System.out.println("All static members are inherited but we can't do this.");
+
+	}
+
+}
