3fdd6950e7abbef9f88ae651ce2d3ee49220d87f
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-10 10:22:17 +0530

    First example code of if Statement was written.

---
+++ Control_Flow_Statement/src/decision_Statement/If1.java
@@ -0,0 +1,17 @@
+package decision_Statement;
+
+public class If1 {
+	
+	public static void main(String[] args) {
+		
+		String study = "12th";
+		if (study=="12th") {
+			
+			System.out.println("Give admission to any degree and study first year");
+		}
+		System.out.println("Study second year");
+		System.out.println("Study third year");
+		System.out.println("Study fourth year");
+	}
+
+}
