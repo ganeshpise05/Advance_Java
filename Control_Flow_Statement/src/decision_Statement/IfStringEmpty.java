8d3df7022506b36f31e1e9954f2177c3a7e2fd4f
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-10 11:22:32 +0530

    Decision statement:If-eg3 ;

---
+++ Control_Flow_Statement/src/decision_Statement/IfStringEmpty.java
@@ -0,0 +1,14 @@
+package decision_Statement;
+
+
+// Checking if a string is empty
+public class IfStringEmpty {
+
+	public static void main(String[] args) {
+		 String str ="";
+		 if (str.isEmpty()) {
+			System.out.println("The given string is empty.");
+		}
+	}
+
+}
