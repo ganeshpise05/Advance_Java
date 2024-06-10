8d0a889d99d9e5f0c67f06c7e1cc5087a1a2ba29
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-10 10:33:33 +0530

    Second eg. of if statement:num positive or negative

---
+++ Control_Flow_Statement/src/decision_Statement/If2.java
@@ -0,0 +1,12 @@
+package decision_Statement;
+
+public class If2 {
+
+	public static void main(String[] args) {
+		int num = 10;
+		if (num>0) {
+			System.out.println("The given number is positive");
+		}
+		System.out.println("The given number was negative.");
+	}
+}
