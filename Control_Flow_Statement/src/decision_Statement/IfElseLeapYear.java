77eb5e57620a0784433634fd2061227d92372b1c
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-10 11:52:47 +0530

    Decision statement:If_else-eg2 ;

---
+++ Control_Flow_Statement/src/decision_Statement/IfElseLeapYear.java
@@ -0,0 +1,18 @@
+package decision_Statement;
+
+public class IfElseLeapYear {
+	 public static void main(String[] args) {
+		
+		 int year = 2020;
+		 if ((year%4==0 && year%100!=0) || year%400==0) {
+			 
+			 System.out.println("The given year is leapyear.");
+			
+		} else {
+			
+			System.out.println("The given year is not leapyear");
+
+		}
+	}
+
+}
