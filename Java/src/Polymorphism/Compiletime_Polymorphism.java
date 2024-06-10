8b954f36369b6960e9770ef69e3dd2559b5917fb
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-10 13:38:48 +0530

    Theory program of Compiletime Polymorphism.

---
+++ Java/src/Polymorphism/Compiletime_Polymorphism.java
@@ -0,0 +1,16 @@
+package Polymorphism;
+
+public class Compiletime_Polymorphism {
+
+	public static void main(String[] args) {
+		 
+		System.out.print("There are Five types of Compiletime Polymorphism :");
+		System.out.println(" Method Overloading");
+		System.out.println("                                                 : Constructor Overloading");
+		System.out.println("                                                 : Method Shadowing");//Method and Variable Shadowing Depends on the concept of Runtime Polymorphism.
+		System.out.println("                                                 : Variable Shadowing");
+		System.out.println("                                                 : Operator Overloading");//not used in java 
+
+	}
+
+}
