914855e5bfefaa13ad62463fcd0be5d23da2f80a
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-10 11:27:16 +0530

    Decision statement:If-eg3 ;

---
+++ Control_Flow_Statement/src/decision_Statement/IfWithinrange.java
@@ -0,0 +1,14 @@
+package decision_Statement;
+
+//Example 4: Checking if a number is within a range
+public class IfWithinrange {
+
+	public static void main(String[] args) {
+		 int num = 12;
+		 if (num>0 && num<20) {
+			System.out.println("The given number is within a range of 0-20.");
+		}
+
+	}
+
+}
