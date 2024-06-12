c8b535b7cb84b8ecd4f42eea74251845ce002468
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-12 19:59:25 +0530

    Nested if-else Example No:2

---
+++ Control_Flow_Statement/src/decision_Statement/ElseIf_AgeGroup.java
@@ -0,0 +1,27 @@
+package decision_Statement;
+
+import java.util.Scanner;
+
+public class ElseIf_AgeGroup {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		System.out.print("Enter a age : ");
+		int age = sc.nextInt();
+		
+		if (age<13) {
+			System.out.println("The person is Child.");
+		} 
+		else if (age<20) {
+			System.out.println("This person is Teenager.");
+		}
+		else if ( age<65) {
+			System.out.println("The given person is Adult");
+		}
+		else {
+            System.out.println("Please enter a valid Age !!");
+		}
+	}
+
+}
