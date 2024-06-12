d5722e6ec5b26478272a3fb20db37d1e8b07c97f
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-12 22:16:33 +0530

    While loop Example No:1

---
+++ Control_Flow_Statement/src/loop_Statement/While_CountofNum.java
@@ -0,0 +1,15 @@
+package loop_Statement;
+//While loop Example No:1
+public class While_CountofNum {
+
+	public static void main(String[] args) {
+		 
+		int i = 1;
+		while (i<=10) {
+			System.out.println(i);
+			i++;
+		}
+		
+	}
+
+}
