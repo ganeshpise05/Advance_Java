bd516bb268b3f4a754c8d783148766a9753b4aed
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-12 22:36:34 +0530

    While loop Example No:4

---
+++ Java/src/Inheritance/While_CharofString.java
@@ -0,0 +1,18 @@
+package Inheritance;
+
+public class While_CharofString {
+
+	public static void main(String[] args) {
+		
+		String name = "Ganesh";
+		 int i =0;
+		
+		
+		    while (i<name.length()) {
+			System.out.println(name.charAt(i));
+			i++;
+		}
+
+	}
+
+}
