d4285ae0ca8891a159d87c0d4e6ff049cbcea09c
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-18 14:36:34 +0530

    Method Shadowing

---
+++ Java/src/Method_Variable_Shadowing/TheoryMethod_Variable_Shadowing.java
@@ -0,0 +1,14 @@
+package Method_Variable_Shadowing;
+
+public class TheoryMethod_Variable_Shadowing {
+
+	public static void main(String[] args) {
+		 
+		System.out.println("Method Shadowing: If we have static methods with same signature in both super and sub class (parent & child).");
+		System.out.println("-----------------<>--------------");
+		System.out.println("The preference is given to the ----parent class-----not a child class.Bcz method shadowing is compiletime polymorphism.");
+		System.out.println("The execution of method defination is does not depend on the type of object created instead it depends on type of referance used to invoke the method. ");
+        
+	}
+
+}
