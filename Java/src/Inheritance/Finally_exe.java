d5f7a4c7601ca79e6e6b4eb4e2e8df242869dced
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-19 22:18:17 +0530

    8.Finally Block

---
+++ Java/src/Inheritance/Finally_exe.java
@@ -0,0 +1,36 @@
+package Inheritance;
+
+public class Finally_exe {
+
+	public static void main(String[] args) {
+		
+		try {
+			String str=null;
+			System.out.println(str); 
+			System.out.println(str.length());
+			
+			
+		} 
+		
+		catch (NullPointerException e) {
+			 System.out.println("please do't initialize var value as null.");
+		}
+		
+		catch (Exception e) {
+			 System.out.println("From exception exe block.");
+		}
+		
+		catch (Throwable te) {
+			 System.out.println("From throwable exception block");
+		}
+		
+		//This is the by default block it execute after try and catch.
+		
+		//When exception occur outside the try and catch block the Finally block executed.
+		//If exception is occured in try block and not handled,but still Finally block get executed.
+		
+		finally {
+			System.out.println("This block can excuted without call or invoke.");
+		}
+	}
+}
