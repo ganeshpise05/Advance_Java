f9f759dbf341222a0f7fc686fb7b98f1290f2a43
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-19 14:17:22 +0530

    While Example No:3-Condition False Stop Loop

---
+++ Control_Flow_Statement/src/loop_Statement/While_ConditionMet.java
@@ -0,0 +1,16 @@
+package loop_Statement;
+
+//Printing a message until a condition is met:
+
+public class While_ConditionMet {
+	
+	public static void main(String[] args) {
+		
+		int count = 0;
+		while (count<5) {
+			System.out.println("condition was not failed..");
+			count++;
+		}
+	}
+
+}
