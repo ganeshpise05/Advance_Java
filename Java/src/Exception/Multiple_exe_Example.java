c3bec8c6d60d15c1e92b6bd8f2f06e19d231fd72
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-19 22:35:15 +0530

    Multiple Exceptions example no:1

---
+++ Java/src/Exception/Multiple_exe_Example.java
@@ -0,0 +1,47 @@
+package Exception;
+
+public class Multiple_exe_Example {
+
+	public static void main(String[] args) {
+		
+		try {
+            // Code that may throw various exceptions
+            String str = null;
+            System.out.println(str.length()); // This will cause NullPointerException
+
+            String invalidNumber = "ABC";
+            int num = Integer.parseInt(invalidNumber); // This will cause NumberFormatException
+
+            int result = 50 / 0; // This will cause ArithmeticException	
+		}
+		
+		catch (NullPointerException e) {
+            System.out.println("NullPointerException caught: " + e);
+            System.out.println("Exception message: " + e.getMessage());
+        } 
+		
+		catch (NumberFormatException e) {
+            System.out.println("NumberFormatException caught: " + e);
+            System.out.println("Exception message: " + e.getMessage());
+        } 
+		
+		catch (ArithmeticException e) {
+            System.out.println("ArithmeticException caught: " + e);
+            System.out.println("Exception message: " + e.getMessage());
+        } 
+		
+		catch (Exception e) {
+			System.out.println("General Exception caught: " + e);
+            System.out.println("Exception message: " + e.getMessage());
+		}
+		
+		catch (Throwable e) {
+			System.out.println("Throwable Exception caught: " + e);
+            System.out.println("Exception message: " + e.getMessage());
+		}
+		
+		finally {
+			System.out.println("This finally block executes by default.");
+		}
+	}
+}
