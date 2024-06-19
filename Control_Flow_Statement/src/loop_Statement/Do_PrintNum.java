06f87a834ab312caf5d3ef34492d498841c6e42e
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-19 15:20:44 +0530

    Do-While Example no:2-Printnumbers

---
+++ Control_Flow_Statement/src/loop_Statement/Do_PrintNum.java
@@ -0,0 +1,15 @@
+package loop_Statement;
+
+public class Do_PrintNum {
+	
+	public static void main(String[] args) {
+		
+		int num=0;
+		
+		do {
+			System.out.println("Hello my name is Funtya.");
+			num++;
+		} while ( num<5);
+	}
+
+}
