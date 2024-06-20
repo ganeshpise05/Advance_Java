7ee6755a47590c70d25cc7f998ec2587d12a8cd1
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-20 22:57:12 +0530

    Wrapper Eg:1

---
+++ Java/src/Wrapper/Wrapper_eg1.java
@@ -0,0 +1,11 @@
+package Wrapper;
+
+public class Wrapper_eg1 {
+
+	public static void main(String[] args) {
+		
+		int j=50;
+		String a = String.valueOf(j);
+		System.out.println(a.toString());
+	}
+}
