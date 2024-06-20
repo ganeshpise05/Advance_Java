43d3b00fc87a9c449d05b9cb9e925ea142262300
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-20 22:57:45 +0530

    Wrapper Eg:2

---
+++ Java/src/Wrapper/Wrapper_eg3.java
@@ -0,0 +1,12 @@
+package Wrapper;
+
+public class Wrapper_eg3 {
+
+	public static void main(String[] args) {
+		
+		Integer a = new Integer(3);
+		byte i = a.byteValue();
+		int j = a;
+		System.out.println(i+" "+j);
+	}
+}
