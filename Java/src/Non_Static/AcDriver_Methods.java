9d4a7f2d178fc8f03d85b215c86d81b126465eac
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-21 15:23:19 +0530

    Non-static methods : Ac Driver

---
+++ Java/src/Non_Static/AcDriver_Methods.java
@@ -0,0 +1,12 @@
+package Non_Static;
+
+public class AcDriver_Methods {
+
+	public static void main(String[] args) {
+		
+		Ac_Methods a1 = new Ac_Methods("Samsung",50000, "White", 5, "Cooling", 67, "Remote");
+		a1.brand();
+		
+		a1.displayAc();
+	}
+}
