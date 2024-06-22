6dce4cd8dc11117afa2e7e0be631e4cf92e296bf
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-22 23:33:23 +0530

    Theory of Constructor & Method overloading

---
+++ Java/src/Method_Constructor_Overloading/TheoryMethod_Constructor_Overloading.java
@@ -0,0 +1,30 @@
+package Method_Constructor_Overloading;
+
+public class TheoryMethod_Constructor_Overloading {
+
+	public static void main(String[] args) {
+		
+		System.out.println("                          *BELOW ALL OPERATION DONE IN SAME CLASS*                              "); 
+		System.out.println("      ");
+		
+		System.out.println("Method Overloading: If class has more than one method of same name with different Formal Argument.");
+		System.out.println("      ");
+		System.out.println("Rule: Method name must be same ");
+		System.out.println("      The formal argument must be different either its length or it's datatype.");
+		System.out.println("      It comes under the compiletime binding or Polymorphism. ");
+		System.out.println("      Here the binding between the method call statement and method body is done by the compiler at compiletime");
+		System.out.println("--------------------------------------------------------------------------------------------------------------------------------------");
+		System.out.println("      ");
+		
+		System.out.println("Constructor Overloading: If a class having more than one constructor having same name but different formal argument is known as Constructor Overloading.");
+		System.out.println("    ");
+		System.out.println("Rule: constructor name should be same as class name. ");
+		System.out.println("      The formal arguments of constructor must be different either its type or no of formal argument.");
+		System.out.println("      When we do constructor overloading when we want to customize the loading of values to the object member.");
+		System.out.println("   -------------------------------------------------------------------------------------------------------------------------------------   ");
+		System.out.println("      ");
+		
+		 
+	}
+
+}
