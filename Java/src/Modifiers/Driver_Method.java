7718e063ea28a200ffa30a344767c22f97d71554
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-24 22:41:40 +0530

    Access Modifier: Final methods Using inheritance

---
+++ Java/src/Modifiers/Driver_Method.java
@@ -0,0 +1,10 @@
+package Modifiers;
+
+public class Driver_Method {
+
+	public static void main(String[] args) {
+		
+		Final_Methods f2 = new Final_MethodsInheritance();
+		Final_Methods.main(args);
+	}
+}
---
+++ Java/src/Modifiers/Final_MethodsInheritance.java
@@ -0,0 +1,19 @@
+package Modifiers;
+
+public class Final_MethodsInheritance extends Final_Methods {
+	
+	public void showFinal() {
+		test();
+		demo();  //We can inherit properties of parent class .
+	}
+	
+	/*
+	public void test() {
+		System.out.println("We can not override the test() method.");       //CTE
+	}                                                                     
+	
+	public static void demo() {
+		System.out.println("We can't override the static method demo().");  //CTE
+	}
+	*/
+}
