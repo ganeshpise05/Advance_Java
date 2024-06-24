724398b9c771dcc6c5291ddfc7d6d2052240129e
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-25 00:23:14 +0530

    For loop:4-Sum of 10 natural numbers

---
+++ Control_Flow_Statement/src/loop_Statement/SumofNaturalNumbers.java
@@ -0,0 +1,15 @@
+package loop_Statement;
+
+public class SumofNaturalNumbers {
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		for (int i = 1; i <=10; i++) {
+			
+			sum+=i;
+		}
+		
+		System.out.print("Sum of first 10 natural number is: "+ sum);
+	}
+
+}
