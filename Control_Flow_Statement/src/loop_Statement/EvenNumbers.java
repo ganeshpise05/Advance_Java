3bc10cc34b4b52c234daddad4e7fa76fb8a16d1d
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-25 00:18:34 +0530

    For loop:2-Even numbers from 0 to 40

---
+++ Control_Flow_Statement/src/loop_Statement/EvenNumbers.java
@@ -0,0 +1,13 @@
+package loop_Statement;
+
+public class EvenNumbers {
+	
+	public static void main(String[] args) {
+		
+		for (int i = 0; i <= 40; i+=2) {
+			
+			System.out.print(i+" ");
+		}
+	}
+
+}
