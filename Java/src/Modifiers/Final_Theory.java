04bd6f0aab60db1b9f7e62d6f42fe7686cb1481f
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-24 20:02:38 +0530

    Access Modifier: Final Theory

---
+++ Java/src/Modifiers/Final_Theory.java
@@ -0,0 +1,27 @@
+
+package Modifiers;
+
+public class Final_Theory {
+
+	public static void main(String[] args) {
+		
+		System.out.println("                               VARIABLES       METHODS           CLASS                    ");
+		System.out.println("   ");
+		
+		System.out.println("*** Final Variables *** : 1> local variable  :   i>  static local variable");
+		System.out.println("                                                 ii> non-static local variable");
+		System.out.println(" ");
+		
+		
+		System.out.println("                          2> Global Variable :   i>  static global variable");
+		System.out.println("                                                 ii> non-static global variable");
+		System.out.println("");
+		
+		
+		System.out.println("*** Final Methods *** :   1> Static Methods");
+		System.out.println("                          2> Non-static Methods");
+		System.out.println("  ");
+		
+		System.out.println("*** Final Class ***");
+	}
+}
