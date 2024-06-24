9b2d77e608e2dc75c7b48156b84b700f60363314
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-25 00:20:08 +0530

    For loop:3-Odd numbers from 1 to 40

---
+++ Control_Flow_Statement/src/loop_Statement/OddNumbers.java
@@ -0,0 +1,12 @@
+package loop_Statement;
+
+public class OddNumbers {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <=40; i+=2) {
+			
+			System.out.print(i+" ");
+		}
+	}
+}
