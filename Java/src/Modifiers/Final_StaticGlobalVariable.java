a3167a55909afe7a54dbf6ab3488c7aa7a94e160
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-24 20:27:04 +0530

    Final Var :Static Global var

---
+++ Java/src/Modifiers/Final_StaticGlobalVariable.java
@@ -0,0 +1,30 @@
+package Modifiers;
+
+public class Final_StaticGlobalVariable {
+	
+	 static final int a = 100;
+//	 static final char ch ;// CTE we can't write any final global var without declaration 	
+	 
+//	 It's possible when this variable declared in static block.
+	 static final String str;
+	 static {
+		 str ="Ganesh";
+	 }
+	
+	public static void main(String[] args) {
+		
+        
+		System.out.println("The global static final var value is: "+a);
+		
+//		a = 120; //CTE We can't reasign variable.
+//		System.out.println(a);
+		
+//		System.out.println("The given character is: "+ch); CTE
+		
+		System.out.println("The given string is declared in static block is: "+str);
+		
+//		str ="Pise";//CTE can't reinitialize.
+//		System.out.println("New string is: "+str);
+	}
+
+}
