f3cb08c55ee3c79ef4797030a990eb2da5a8e9a9
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-28 15:44:06 +0530

    7:Reverse array-Unchanged positions of elements

---
+++ Java/src/Arrays/Unchanged_PositionElementsAfterReverse.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+
+//7:Check the elements in given array if the array is reversed but unchanged position elements print.
+
+public class Unchanged_PositionElementsAfterReverse {
+
+	public static void main(String[] args) {
+		
+		int[] a = {1,2,4,5,4,2,8};
+		
+	}
+}
