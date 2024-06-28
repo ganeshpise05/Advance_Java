787985a71adb3e645661b21ddd8cf47890335411
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-28 15:22:06 +0530

    5:Reverse array by itself.

---
+++ Java/src/Arrays/Reverse_Itself.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+import java.util.Arrays;
+
+//5:Reverse the array by itself.
+public class Reverse_Itself {
+
+	public static void main(String[] args) {
+		
+		int [] a = {10,20,40,30,20,70};
+		
+		for (int i = 0,j = a.length-1; i < j; i++,j--) {
+			
+			int temp = a[i];
+			a[i] = a[j];
+			a[j] = temp;
+		}
+		
+		System.out.println("The reverse array of given array by itself is: "+ Arrays.toString(a));
+	}
+}
