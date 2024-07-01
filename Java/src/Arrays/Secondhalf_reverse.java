dccb7c924520bf9fbc126d29f6ce45abb0aa9997
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-01 23:50:41 +0530

    .

---
+++ Java/src/Arrays/Secondhalf_reverse.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+/*   9: Give an a array
+ *   first half must be as it is
+ *   second half is in reverse order  */
+public class Secondhalf_reverse {
+
+	public static void main(String[] args) {
+		
+		int [] a = {1,2,3,4,5,6,7,8};
+		
+		
+	}
+}
