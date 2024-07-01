7666d65c4016b678798f52873fc6c187e5f10bfa
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-01 23:51:33 +0530

    .

--- Java/src/Arrays/Zigzag_CopyElements.java
+++ Java/src/Arrays/Zigzag_CopyElements.java
@@ -2,28 +2,27 @@ package Arrays;
 
 import java.util.Arrays;
 
-//Zigzag Array:Copy the elements from two arrays like one from 1st array and another one from 2nd array. 
+//Zigzag Array Even size:Copy the elements from two arrays like one from 1st array and another one from 2nd array . 
 public class Zigzag_CopyElements {
 	
 	public static void main(String[] args) {
 		
 		int [] a = {10,20,30,40,50,60};
-		int [] b = {90,80,70,60,50,40};
+		int [] b = {1,2,3,4,5,6};
 		int [] ans = new int [a.length+b.length];
 		
-		for (int i = 0,j=0; i < ans.length; i+=2,i++) {
+		for (int i = 0; i < ans.length; i++) {
 			
-	
-		for (int i = 0; i < a.length; i++) {
+	      if (i==0 || i%2==0) {
 			ans[i] = a[i];
+		} 
+	      
+	      else {
+              int j = 0;
+              ans[i] = b[j];
+              j++;
+		  }
 		}
-		
-		for (int i=a.length ,j = 0;i < ans.length;i++,j++) {
-		    ans[i] = b[i];	
-		}
-		ans[i] = a [j];
-		ans[i+1] = b[j];
-	   }
 		System.out.println("When above two arrays are merged then their combined array is: "+Arrays.toString(ans));
 	}
 
