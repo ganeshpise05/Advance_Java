31ba24bc5e82785e9302903c628f298e18e87d91
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-02 14:50:06 +0530

    Printing Positive and negative elements of array.

---
+++ Java/src/Arrays/PositiveNegativeElementsPrint.java
@@ -0,0 +1,27 @@
+package Arrays;
+ 
+
+//Give an array print the all positive and negative elements.
+public class PositiveNegativeElementsPrint {
+
+	public static void main(String[] args) {
+		
+		int [] a = {1,-3,6,-67,-34,78,4,920,-234,67,0};
+		
+		for (int i = 0; i < a.length; i++) {
+			
+			if (a[i]>0) {
+				System.out.print(a[i]+" ");
+			} 
+		}
+		
+		for (int i = 0; i < a.length; i++) {
+			
+			if (a[i]<0) {
+				System.out.print(a[i]+" ");
+			}
+		}
+		
+		
+	}
+}
