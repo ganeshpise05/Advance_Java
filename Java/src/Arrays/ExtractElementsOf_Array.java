a2b0235b9ff2d6401bac93e41ea94303d0c2f2cf
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-02 14:29:40 +0530

    Extract one one elements from Array.

---
+++ Java/src/Arrays/ExtractElementsOf_Array.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+// Give an array extract the elements and print.
+public class ExtractElementsOf_Array {
+
+	public static void main(String[] args) {
+		
+		int a [] = {5,7,895,3,9,0,2};
+		
+		for (int i = 0; i < a.length; i++) {
+			
+			System.out.println(a[i]);
+		}
+	}
+}
