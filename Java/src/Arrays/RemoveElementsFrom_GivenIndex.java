785472edb15ddd3e97cb2b38fab3f89cd6dbe1ff
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-05 23:31:47 +0530

    Remove Elements From given index 3 to 6.

---
+++ Java/src/Arrays/RemoveElementsFrom_GivenIndex.java
@@ -0,0 +1,32 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+// Remove Elements : Give an one array and remove the elements from index 3 to index 6.(It not consider 6th index element)
+// [1,2,3,4,5,6,7,8,9]  ---------->  [1,2,3,7,8,9]
+
+public class RemoveElementsFrom_GivenIndex {
+
+	public static void main(String[] args) {
+		
+		int start = 3;
+		int end = 6;
+		int diff = end - start; 
+		
+		int a [] = {1,2,3,4,5,6,7,8,9};
+		int ans [] = new int [a.length-diff];
+		
+		for (int i = 0; i < ans.length; i++) {
+			if (i<start) {
+				ans[i] = a[i];
+			}
+			else if (i>=end) {
+                ans[i-diff] = a[i];
+                ans[i] = ans[i-diff];
+			}
+		}
+		
+		System.out.println(Arrays.toString(ans));
+	}
+}
