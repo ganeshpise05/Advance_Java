615973739f00eb6ebaf9e902a5f67a19e03869c4
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-06 22:26:31 +0530

    Remove Elements: Odd Index

--- Java/src/Arrays/RemoveElements_OddIndex.java
+++ Java/src/Arrays/RemoveElements_OddIndex.java
@@ -8,17 +8,16 @@ public class RemoveElements_OddIndex {
 
 	public static void main(String[] args) {
 		
-		int a [] = {1,2,3,4,5,6,7,8,9};
-		// Giving size for new array
-		int newSize = (a.length + 1) / 2;
-		int ans[] = new int[newSize];
+		int a [] = {1,2,3,4,5,6,7,83,9,10,11,12,13,17,41,53,1};
+		// Giving size for new array 
+		int ans[] = new int[a.length/2];
 		 
 		for (int i = 0,j = 0; i < a.length; i++) {
-			if (i%2 == 0) {
+			if (i % 2 != 0) {
 				ans[j] = a[i];
 				j++;
 			}  
 		}
-		System.out.println(Arrays.toString(ans));
+		System.out.println("Odd index elements removed array is: "+Arrays.toString(ans));
 	}
 }
