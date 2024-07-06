9985cb6f15acad4ed63733a709ee1d89daf62ce8
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-06 23:44:00 +0530

    Remove element.

--- Java/src/Arrays/RemoveElementsFrom_GivenIndex2.java
+++ Java/src/Arrays/RemoveElementsFrom_GivenIndex2.java
@@ -1,8 +1,6 @@
 package Arrays;
 
-import java.util.Arrays;
-import java.util.Iterator;
-
+import java.util.Arrays; 
 // Remove Elements of Given array: Give an a array and remove the elements from the index 6 to 10.
 // [1,2,3,4,5,6,7,8,9,10,11,12,13]  -------> [1,2,3,4,5,6,11,12,13]
 
