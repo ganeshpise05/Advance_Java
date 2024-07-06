e5d31d83d00f82688a5d0e99a141bd20adfe736d
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-06 23:38:03 +0530

    Count of repeated elements.

---
+++ Java/src/Abstraction/Countof_RepeatedElements.java
@@ -0,0 +1,37 @@
+package Abstraction;
+
+public class Countof_RepeatedElements {
+
+	public static void main(String[] args) {
+		
+		int a [] = {1,2,4,2,4,1,5,6,7,4,3,6,7,8,95,6,3,11,2,34,2,7,43};
+		int ref [] = new int [a.length];
+		
+		int count;
+		int count2 =0;
+		for (int i = 0; i < a.length; i++) {
+			
+			if (ref[i] == 0) {
+				count =1;
+				
+				for (int j = i+1; j < a.length; j++) {
+					
+					if (a[i] == a[j]) {
+						count++;
+						ref[j] = -1;
+					}
+				}
+				ref[i] = count;
+			}
+		}
+		
+		for (int i = 0; i < ref.length; i++) {
+			if (ref[i] != -1) {
+				count2 ++;
+				ref[i] = count2;
+			}
+			System.out.println("count of "+ref[i]+" is: "+count2);
+		}
+		
+	}
+}
