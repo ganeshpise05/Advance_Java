00c0ff1d51f25254c2e17446bc776389b6d6a94b
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-09 23:53:19 +0530

    For loop: Printing numbers

---
+++ Control_Flow_Statement/src/loop_Statement/For_PrintNumbers.java
@@ -0,0 +1,13 @@
+package loop_Statement;
+ 
+
+public class For_PrintNumbers {
+
+	public static void main(String[] args) {
+		
+		int a = 0;
+		 for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+	}
+}
