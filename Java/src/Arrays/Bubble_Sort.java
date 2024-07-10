3e83e4f6b6368f20c379dc8665b1bf226c9ee259
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-10 13:38:16 +0530

    Practice:Bubble Sort

---
+++ Java/src/Arrays/Bubble_Sort.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+import java.util.Arrays;
+
+// Bubble Sort: Give one array and sort elements by using the asc order.
+// [9,8,7,6,5,4,3,2,1] -----> [1,2,3,4,5,6,7,8,9]
+
+public class Bubble_Sort {
+
+	public static void main(String[] args) {
+		
+		int a [] = {9,8,7,6,5,4,3,2,1};
+		for (int i = 0; i < a.length-1; i++) {
+			for (int j = 0; j < a.length-1-i; j++) {
+				if (a[j]>a[j+1]) {
+					int temp = a[j];
+					   a[j] = a[j+1];
+					   a[j+1] = temp;
+				}
+			}
+		}
+		System.out.println("Bubble Sorted Array: "+Arrays.toString(a));
+	}
+}
