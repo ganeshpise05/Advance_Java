4ca20b6ef12d867a6587ecd98878ca07c36e24cc
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-12 23:41:39 +0530

    Switch case : Day

---
+++ Java/src/Statements/Day.java
@@ -0,0 +1,51 @@
+package Statements;
+
+import java.util.Scanner;
+
+// Example: Day of the Week
+public class Day {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a day: ");
+		int day = sc.nextInt();
+		
+		String dayName = null;
+		
+		switch (day) {
+		    case 1:
+			  dayName = "Monday";
+			  break;
+			  
+		    case 2:
+		    	dayName = "Tuesday";
+		    	break;
+		    	
+		    case 3:
+		    	dayName = "Wednesday";
+		    	break;
+		    	
+		    case 4:
+		    	dayName = "Thursday";
+		    	break;
+		    	
+		    case 5:
+		    	dayName = "Friday";
+		    	break;
+		    	
+		    case 6:
+		    	dayName = "Saturday";
+		    	break;
+		    	
+		    case 7:
+		    	dayName = "Sunday";
+		    	break;
+
+		default:
+			System.out.println("Please enter a valid Day.");
+			break;
+		}
+		  System.out.println(dayName);
+	}
+}
