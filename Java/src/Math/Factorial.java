1d008ba898c495cf2bef2941260dc1deedd66520
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-14 23:29:14 +0530

    Calculating Factorial

---
+++ Java/src/Math/Factorial.java
@@ -0,0 +1,23 @@
+package Math;
+
+import java.util.Scanner;
+
+// Calculating Factorial: n! = n X (n-1) X ..... X 1
+
+public class Factorial {
+
+	public static void main(String[] args) {
+        
+		Scanner sc = new Scanner (System.in);
+		System.out.print("Enter a number: ");
+		int n = sc.nextInt();
+		
+        int factorial = 1;
+
+        for (int i = 1; i <= n; ++i) {
+            factorial *= i;
+        }
+
+        System.out.println("Factorial of " + n + " = " + factorial);
+    }
+}
