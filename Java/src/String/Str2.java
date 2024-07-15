64e8b98ccb80ab8eaefeab42d7576cbb68d1a833
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-15 22:29:53 +0530

    String quiz.

---
+++ Java/src/String/Str2.java
@@ -0,0 +1,14 @@
+package String;
+
+public class Str2 {
+
+	public static void main(String[] args) {
+		
+		 String obj = "hello";
+		 String obj1 = "world";
+		 String obj2 = obj;
+		 obj2 = "world";
+		 
+		 System.out.println(obj + " "+obj2);
+	}
+}
