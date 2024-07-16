fdd0aaaf3ba1bd0cd695c17cbf04cce024fbd019
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-16 19:47:51 +0530

    1:Extract characters of String.

---
+++ Java/src/String/ExtractChar_1.java
@@ -0,0 +1,15 @@
+package String;
+
+// Extract characters : Give one string and extract the Characters of given String.
+// String s ="Funtya" ---------> F u n t y a
+
+public class ExtractChar_1 {
+
+	public static void main(String[] args) {
+		
+		String s = "Funtya";
+		for (int i = 0; i < s.length(); i++) {
+			System.out.print(s.charAt(i)+" ");
+		}
+	}
+}
