7ce2cf2812a27703cbe4f9aae2202578d06db290
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-17 14:18:03 +0530

    Bultin Methods in String class.

---
+++ Java/src/String/Str5.java
@@ -0,0 +1,41 @@
+package String;
+
+public class Str5 {
+
+	public static void main(String[] args) {
+		
+//		String s1 = "Hello Good Morning";
+//		String str = s1.replace("Hello", "Hi");
+//		System.out.println( str);
+		
+//		String s1  = "good";
+//		System.out.println(s1.indexOf("od"));
+		
+//		String s1  = "malayalam";
+//		System.out.println(s1.indexOf('l',2));
+		
+//		String s1 = "computer";
+//		System.out.println(s1.indexOf('m'));
+		
+//		String s1 = "SITHA";
+//		String s2 = "RAMA";
+//		System.out.println(s1.charAt(0)>s2.charAt(0));
+		
+//		String s1 = "computer";
+//		System.out.println(s1.indexOf('z'));
+		
+//		String s1 = "computer";
+//		System.out.println(s1.charAt('a'));
+		
+
+//		String s1 = "computer";
+//		System.out.println(s1.charAt(s1.length()-1));
+		
+
+		String s1 = "computer";
+		System.out.println(s1.charAt(2));
+		
+		
+		
+	}
+}
