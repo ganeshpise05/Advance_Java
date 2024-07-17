e2f7efb667fab35e997f5a21a8aa3b03d2df8259
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-17 23:19:18 +0530

    Test 3

---
+++ Java/src/String/Str1.java
@@ -0,0 +1,11 @@
+package String;
+
+public class Str1 {
+
+	public static void main(String[] args) {
+		
+		String s = new String("Hello");
+		char[] c =  s.toCharArray();
+		System.out.println(c);
+	}
+}
