b63e488b8ca0b0aa2918836a1a6744cb055a5f83
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-18 23:27:54 +0530

    .

---
+++ Control_Flow_Statement/src/decision_Statement/IfElseVoting.java
@@ -0,0 +1,21 @@
+package decision_Statement;
+
+//Example 3: Checking if a person is eligible to vote
+public class IfElseVoting {
+
+	public static void main(String[] args) {
+		 
+		int age = 20;
+		if (age>=18) {
+			
+			System.out.println("You are eligible for voting.");
+			
+		} else {
+			
+			System.out.println("You are not eligible for voting.");
+
+		}
+
+	}
+
+}
