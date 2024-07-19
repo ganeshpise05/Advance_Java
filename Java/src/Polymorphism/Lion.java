e27455293dc2a8f1bdcc5e2b34af3784243d1193
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-19 22:50:09 +0530

    Lion extends Animal.

---
+++ Java/src/Polymorphism/Lion.java
@@ -0,0 +1,15 @@
+package Polymorphism;
+
+public class Lion extends Animal{
+
+	public static void sound()
+	{
+		System.out.println("Lion Roars.");
+	}
+	
+	public static void legs()
+	{
+		System.out.println("lion have four legs.");
+	}
+	
+}
