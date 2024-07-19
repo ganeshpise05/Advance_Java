21d04cc7b056ee0e9454c3f6e79dcde37fe60bbb
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-19 23:05:37 +0530

    Polymorphism: Super class Shape.

---
+++ Java/src/Polymorphism/Shape.java
@@ -0,0 +1,14 @@
+package Polymorphism;
+
+public class Shape {
+
+	public void shape()
+	{
+		System.out.println("Drawing the shape.");
+	}
+	
+	public void area()
+	{
+		System.out.println("Find the area.");
+	}
+}
