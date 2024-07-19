634fc144c66ae9f541da92ac8cc319f21dcd32da
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-19 23:12:04 +0530

    Rectangle extends Shape.

---
+++ Java/src/Polymorphism/Rectangle.java
@@ -0,0 +1,14 @@
+package Polymorphism;
+
+public class Rectangle extends Shape {
+
+	public void shape()
+	{
+		System.out.println("Shape of rectangle is Rectangular.");
+	}
+	
+	public void area()
+	{
+		System.out.println("Area of rectangle is: length x breadth");
+	}
+}
