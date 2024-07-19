c5fa1ad9e26cbeb3b4500473e0b0037e89b24006
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-19 23:08:21 +0530

    Circle extends Shape.

---
+++ Java/src/Polymorphism/Circle.java
@@ -0,0 +1,14 @@
+package Polymorphism;
+
+public class Circle extends Shape { 
+		
+    	 public void shape()
+    	 {
+    		 System.out.println("Circle has circular shape.");
+    	 }	
+    	 
+    	 public void area()
+    	 {
+    		 System.out.println("Area of circle is: 2pi X r^2");
+    	 }
+}
