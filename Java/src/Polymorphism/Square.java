40b1d65eeb19423ee6b25695efcdada22a5279b8
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-19 23:10:18 +0530

    Square extends Shape.

---
+++ Java/src/Polymorphism/Square.java
@@ -0,0 +1,14 @@
+package Polymorphism;
+
+public class Square extends Shape {
+
+	public void shape()
+	{
+		System.out.println("Shape of Square is square.");
+	}
+	
+	public void area()
+	{
+		System.out.println("Area of Square is: l^2");
+	}
+}
