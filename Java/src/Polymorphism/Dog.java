c449dcba6aad90e11db7ea22bf6927fe9d0f989b
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-19 22:51:22 +0530

    Dog extends Animal.

---
+++ Java/src/Polymorphism/Dog.java
@@ -0,0 +1,14 @@
+package Polymorphism;
+
+public class Dog {
+
+	public static void sound()
+	{
+		System.out.println("Dog barks loudly.");
+	}
+	
+	public static void legs()
+	{
+		System.out.println("Dog have four legs.");
+	}
+}
