792595a897d580d668c2ab10353ad5d9e74a8136
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-20 22:35:18 +0530

    String Class.

---
+++ Java/src/String/String_Class.java
@@ -0,0 +1,15 @@
+package String;
+
+public class String_Class {
+
+	public static void main(String[] args) {
+		
+		String s1 = new String("Ganesh");
+		
+		String s2 = s1.concat(" Pise");
+		
+		System.out.println(s1);
+		
+		System.out.println(s2);
+	}
+}
