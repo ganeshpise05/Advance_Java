05835bc06600d62a31461ca07a35f0101069a349
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-21 14:52:27 +0530

    .

---
+++ Java/src/Exception/Test3.java
@@ -0,0 +1,21 @@
+package Exception;
+
+public class Test3 {
+	
+	public static void main(String[] args) {
+		
+		 
+			
+			try {
+				  throw new NullPointerException("Hello");
+				  //System.out.println("A");
+				}
+				
+			 catch ( ArithmeticException e) {
+				 System.out.println("B");
+			}
+		 
+}		
+}
+
+
