bafff3e55efc556c6fcd04a26758c2f57db82981
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-21 14:53:06 +0530

    .

--- Java/src/Exception/Multiple_exe_Example.java
+++ Java/src/Exception/Multiple_exe_Example.java
@@ -4,6 +4,8 @@ public class Multiple_exe_Example {
 
 	public static void main(String[] args) {
 		
+		//System.out.println(10/0);
+		 
 		try {
             // Code that may throw various exceptions
             String str = null;
