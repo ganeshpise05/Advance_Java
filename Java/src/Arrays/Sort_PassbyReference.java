3adfd18a6a93d5f633d5afc162ebdfd71c503e42
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-22 21:58:50 +0530

    .

--- Java/src/Arrays/Sort_PassbyReference.java
+++ Java/src/Arrays/Sort_PassbyReference.java
@@ -14,7 +14,6 @@ public class Sort_PassbyReference {
 	}
 	public static void sort(int [] a)
 	{
-//		int a[] = {9,8,7,6,5,4,3,2,1};
 		for (int i = 0; i < a.length-1; i++) {
 			for (int j = 0; j < a.length-1-i; j++) {
 				if (a[j]>a[j+1]) {
