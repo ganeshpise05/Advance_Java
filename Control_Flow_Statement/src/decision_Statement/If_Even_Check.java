68c0558ad77a0c3566d91d93ac4be4f4ad4aa536
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-24 23:24:57 +0530

    Decision Statement: If statement.

---
+++ Control_Flow_Statement/src/decision_Statement/If_Even_Check.java
@@ -0,0 +1,11 @@
+package decision_Statement;
+
+public class If_Even_Check {
+             public static void main(String[] args) {
+				int num = 7;
+				if (num%10==0) {
+					System.out.println("The given number is even");
+				}
+				System.out.println("The given number is odd");
+			}
+}
