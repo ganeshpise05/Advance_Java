4a86344981c9e9e1eb7b3531ee0e78309b709a02
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-25 23:24:00 +0530

    .

--- Java/src/Statements/If2.java
+++ Java/src/Statements/If2.java
@@ -2,4 +2,11 @@ package Statements;
 
 public class If2 {
 
+	public static void main(String[] args) {
+		
+		int marks =35;
+		if (marks>=35) {
+			System.out.println("You are just pass exam.");
+		}
+	}
 }
