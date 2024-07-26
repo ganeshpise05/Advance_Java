df08f34d6b76f162f8f4d3847d5a3f34791a50c7
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-26 23:21:24 +0530

    .

--- Java/src/Abstraction/Vehicle.java
+++ Java/src/Abstraction/Vehicle.java
@@ -7,4 +7,8 @@ public  abstract class Vehicle {
 	public abstract void accelerate();
 	public abstract void brake();
 	public abstract void fuel();
+	public void detail()
+	{
+		System.out.println("This are details of Vehicle.");
+	}
 }
