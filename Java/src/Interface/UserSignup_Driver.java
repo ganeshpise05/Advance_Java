d69df34d8c6aa085461d77816621d49f00a38f18
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-26 23:21:10 +0530

    .

--- Java/src/Interface/UserSignup_Driver.java
+++ Java/src/Interface/UserSignup_Driver.java
@@ -5,15 +5,15 @@ public class UserSignup_Driver {
 	public static void main(String[] args) {
 		 User u1 = new Login();
 		 u1.register("ganeshpise05","Ganesh@2205", "piseganesh2205@gmail.com");
-		 u1.emailid("piseganesh2205@gmail.com");
+//		 u1.emailid("piseganesh2205@gmail.com");
 //		 u1.login("ganeshpise05", "Ganesh2205");
 //		 u1.login("ganeshpise05", "Ganesh@2505");
 //		 u1.login("ganeshpise05","Ganesh@2205");
 //		 u1.login("ganeshpise05","Ganesh@2205");
-		 u1.login("ganeshpise05","Ganesh@2205");
-		 u1.resetPassword("piseganesh2205@gmail.com");
-		 u1.logout();
-		 u1.changePassword("ganeshpise05", "Ganesh@2205", "Funtya@123");
+//		 u1.login("ganeshpise05","Ganesh@2205");
+//		 u1.resetPassword("piseganesh2205@gmail.com");
+//		 u1.logout();
+//		 u1.changePassword("ganeshpise05", "Ganesh@2205", "Funtya@123");
 	}
 
 }
