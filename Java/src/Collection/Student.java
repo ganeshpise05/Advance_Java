28309b6fc812a0eef3861ad1fc4d6047cd0ca2bf
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-29 22:04:02 +0530

    toString method over rided.

--- Java/src/Collection/Student.java
+++ Java/src/Collection/Student.java
@@ -7,18 +7,24 @@ public class Student {
 	String name;
 	int id;
 	String stream;
-	int std;
+	String std;
 	String email;
 	
 	public Student() {
 		
 	}
 	
-	public Student(String name,int id,String stream,int std,String email) {
+	public Student(String name,int id,String stream,String std,String email) {
 		this.name = name;
 		this.id = id;
 		this.stream = stream;
 		this.std = std;
 		this.email = email;
 	}
+	
+	 // Override toString() method to provide a meaningful representation
+    @Override
+    public String toString() {
+        return "Student{name='" + name + "', id=" + id + ", stream='" + stream + "', std='" + std + "', email='" + email + "'}";
+    }
 }
