257d13279f18df174ba4c9848cbfebc72f219490
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-29 21:50:51 +0530

    Blueprint of Student.

---
+++ Java/src/Collection/Student.java
@@ -0,0 +1,24 @@
+package Collection;
+
+// Create Blueprint of Student & add elements by creating an a object.
+
+public class Student {
+
+	String name;
+	int id;
+	String stream;
+	int std;
+	String email;
+	
+	public Student() {
+		
+	}
+	
+	public Student(String name,int id,String stream,int std,String email) {
+		this.name = name;
+		this.id = id;
+		this.stream = stream;
+		this.std = std;
+		this.email = email;
+	}
+}
