5e93d0aa6da7de5a19a297d7866bd986da4cdd94
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-29 12:26:34 +0530

    Collection: Theory of collection.

---
+++ Java/src/Collection/Collection_Theory.java
@@ -0,0 +1,39 @@
+package Collection;
+
+public class Collection_Theory {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Collection are the Set of predefined Classes & Interfaces that help us to programmer to perform different kinds of data structure operations");
+		
+		System.out.println(" ");
+		
+		System.out.println(" Operations performed in DataStructure: ");
+		System.out.println("                                        1> Sorting");
+		System.out.println("                                        2> Searching");
+		System.out.println("                                        3> Traversing");
+		System.out.println("                                        4> Processing Data Efficiently");
+		
+		System.out.println(" ");
+		
+		System.out.println("                                   It has main two classes  (Iterable & Collection                      "); 
+		System.out.println(" ");
+		
+		System.out.println( "                                              Iterable <interface>                              "); 
+		System.out.println(" ");
+		
+		System.out.println( "                                             Collection  <interface>                             "); 
+		System.out.println(" ");
+		
+		System.out.println( "           Set <interface>                         List <interface>                                Queue <interface>   "); 
+		System.out.println(" ");
+		
+		System.out.println("      HashSet  +  SortedSet <interface>       ArrayList    +     Vector                    LinkedList   +   PriorityQueue   "); 
+		System.out.println(" ");
+		
+		System.out.println("   LinkedHashSet  + NavigableSet <interface>                                                  ");
+		System.out.println("");
+		
+		System.out.println("                          TreeSet");
+	}
+}
