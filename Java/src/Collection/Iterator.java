c10d0862d09c04ba5ff264a5dc515a0bfe251496
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-31 22:13:15 +0530

    Iterator theory.

---
+++ Java/src/Collection/Iterator.java
@@ -0,0 +1,10 @@
+package Collection;
+
+public class Iterator {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Iterator: Iterator is used to Iterate over collection");
+		System.out.println("It has Two abstarct methods : next() & hasNext()");
+	}
+}
