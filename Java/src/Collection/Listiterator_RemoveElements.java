8aa5a846d8ec9e3263080fe8875e4e4b242494ca
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-31 23:33:28 +0530

    ListIterator: Remove elements which are start with 'o'.

---
+++ Java/src/Collection/Listiterator_RemoveElements.java
@@ -0,0 +1,34 @@
+package Collection;
+
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+// Remove elements: Remove the elements from given collection by using List Iterator.
+
+public class Listiterator_RemoveElements {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> fruit = new ArrayList<String>();
+		fruit.add("Cherry");
+		fruit.add("Mango");
+		fruit.add("Banana");
+		fruit.add("Orange");
+		fruit.add("Tomato");
+		fruit.add("Potato");
+		fruit.add("Chilli");
+		fruit.add("Lemon");
+		fruit.add("Avocado");
+		fruit.add("Broccoli");
+		System.out.println(fruit);
+		
+		ListIterator<String> s = fruit.listIterator();
+		while (s.hasNext()) {
+			String a = s.next();
+			if (a.startsWith("o")||a.endsWith("o") || a.startsWith("O")||a.endsWith("O")) {
+				s.remove();
+			}
+		}
+		System.out.println(fruit);
+	}
+}
